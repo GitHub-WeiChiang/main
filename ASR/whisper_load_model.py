eab75b59a3d6e1236c2a78133aa326524e835ea5
GitHub-WeiChiang <albert0425369@gmail.com>
2025-10-05 15:27:04 +0800

    🚧 Add all Whisper model size options

--- ASR/whisper_load_model.py
+++ ASR/whisper_load_model.py
@@ -4,7 +4,7 @@ import whisper
 if __name__ == "__main__":
     os.environ["XDG_CACHE_HOME"] = "./model"
 
-    model_sizes = ["small", "medium", "large"]
+    model_sizes = ["tiny", "base", "small", "medium", "large"]
 
     for model_size in model_sizes:
         whisper.load_model(model_size)
